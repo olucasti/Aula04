f2a54b777471de1553d0c4310c6f0038ee96e045
Lucas da Silva Potrich <silva.lucasrs@gmail.com>
2021-08-12 14:33:42 -0300

    Testes validações quando o usuario clica no cancel do JOptionPane, criado método somenteTextos para validar se o usuário indicou algo diferente

--- src/Ex07.java
+++ src/Ex07.java
@@ -4,16 +4,31 @@ import javax.swing.JOptionPane;
 
 public class Ex07 {
 
+	public static boolean somenteTextos(String text) {
+	    return text.matches("[^\\d]+"); //matches no regex
+	    //Se tiver número na string irá retornar falso
+	    // \\  uma sendo obrigatória para servir de caractere de escape
+	}
+
+	
+	
 	public static void main(String[] args) {
-		
+	
 		Scanner leitor = new Scanner (System.in);
 		int feijoesNoJarro = 0;
-				String entrada ;
+		String entrada ;
 	    JOptionPane.showMessageDialog(null, "Você tem a chance de passar uma noite no castelo da princesa Javarella!! Basta acertar o número de feijões no jarro.","Atenção..Novidade, basta acertar a próxima pergunta", JOptionPane.QUESTION_MESSAGE);
 		
 		do {
-			entrada = JOptionPane.showInputDialog("Quantos feijoes tem no Jarro? ");
-			feijoesNoJarro = Integer.parseInt(entrada);
+			entrada = JOptionPane.showInputDialog("Quantos feijoes tem no Jarro? ");	
+		    if (entrada == null) {
+		    	 entrada = "";
+		    } else if (entrada.equals("")  | (somenteTextos(entrada))) {
+			    JOptionPane.showMessageDialog(null, "São permitidos somente números");
+			} else {
+				 feijoesNoJarro = Integer.parseInt(entrada);
+			}
+			
 		}while (feijoesNoJarro != 1238);
 		  JOptionPane.showMessageDialog(null, "ACERTOU! Bem vindo ao Javarella rsrs","Acertou", JOptionPane.INFORMATION_MESSAGE);
 				
